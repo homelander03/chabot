c739ddb1e0c3a3acda8cbf99338bdace16d5903f
unknown <mohanstriker03@gmail.com>
2018-07-04 20:56:53 +0530

    Enabling Functionality

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/',function(req,res)
 {
 	res.send("Hi Iam a chatbot")
 })
-
+token = "EAADvmNrEZB44BAInhtzSC3M81AgOMylaCtIdmpy0NuPUi2SdHk9UgBkcePB1SdIQuhYvjIyqdS8fsjSkppZCCbqhucoSfW53icBQZCZBt88fVN4tQEUXKWY21cZAKZBi47Sfbz48wD98IW2HS4OS6xZAii1cIm5guqoXJmUEyPK8AZDZD";
 //FACEBOOK
 app.get('/webhook/',function(req,res)
 {
@@ -28,6 +28,41 @@ app.get('/webhook/',function(req,res)
 	}
 	res.send("Wrong Token")
 })
-
+app.post('/webhook/',function(req,res)
+{
+	let messaging_events = req.body.entry[0].messaging_events;
+	for(let i=0 ; i<messaging_events.length ; i++)
+	{
+		let event = messaging_events[i];
+		let sender = event.sender.id;
+		if(event.messaging && event.message.text)
+		{
+			let text = event.message.text;
+			sendText(sender, "Text echo: " + text.substring(0,100))
+		}
+	}
+	res.sendStatus(200)
+})
+function sendText(sender,text)
+{
+	let messageData = {text:text};
+	request({
+		url:"https://graph.facebook.com/v2.6/me/messages",
+		qs : {access:token,token},
+		method : "POST",
+		json:{
+			receipt : {id: sender},
+			message : messageData
+		}
+	}, function(error , response ,body){
+		if(error){
+			console.log("sending error");
+		}
+		else if(response.body.error)
+		{
+			console.log("response body error");
+		}
+	})
+}
 //Listening the requests
 app.listen( process.env.PORT || 1299);
